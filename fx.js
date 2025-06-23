e2080b0f847fa087a0290580198610ae429aea8d
immisterio <96652250+immisterio@users.noreply.github.com>
2025-06-23 18:11:30 +0300

    Update fx.js

--- fx.js
+++ fx.js
@@ -8,15 +8,11 @@
     Lampa.Storage.set('fxapi_uid', unic_id);
   }
   
-  var proxy_url = 'https://api.allorigins.win/raw?url=';
+  var proxy_url = 'http://cors.cfhttp.top/';
   var api_url = 'http://filmixapp.cyou/api/v2/';
   var dev_token = 'user_dev_apk=2.0.1&user_dev_id=' + unic_id + '&user_dev_name=Lampa&user_dev_os=11&user_dev_vendor=FXAPI&user_dev_token=';
   var modalopen = false;
   var ping_auth;
-  
-  function toProxy(url) {
-	  return 'https://api.allorigins.win/raw?url=' + encodeURIComponent(url)
-  }
 
   function fxapi(component, _object) {
     var network = new Lampa.Reguest();
@@ -55,7 +51,7 @@
       ping_auth = setInterval(function() {
         network.clear();
         network.timeout(8000);
-        network.silent(toProxy(Lampa.Utils.addUrlComponent(api_url + 'user_profile', dev_token + user_token)), function(json) {
+        network.silent(Lampa.Utils.addUrlComponent(api_url + 'user_profile', dev_token + user_token), function(json) {
           if (json && json.user_data) {
             Lampa.Modal.close();
             clearInterval(ping_auth);
@@ -100,7 +96,7 @@
       url = Lampa.Utils.addUrlComponent(url, 'story=' + encodeURIComponent(query));
       url = Lampa.Utils.addUrlComponent(url, dev_token + fxapi_token);
       network.clear();
-      network.silent(toProxy(url), function(json) {
+      network.silent(url, function(json) {
         var cards = json.filter(function(c) {
           c.year = parseInt(c.alt_name.split('-').pop());
           return c.year > year - 2 && c.year < year + 2;
@@ -122,14 +118,14 @@
     };
 
     this.find = function(filmix_id) {
-      var url = api_url;
+      var url = proxy_url + api_url;
 
       end_search(filmix_id);
 
       function end_search(filmix_id) {
         network.clear();
         network.timeout(10000);
-        network.silent(toProxy(url + 'post/' + filmix_id + '?' + dev_token + fxapi_token), function(found) {
+        network.silent(url + 'post/' + filmix_id + '?' + dev_token + fxapi_token, function(found) {
           if (found && Object.keys(found).length) {
             success(found);
             component.loading(false);
@@ -1425,7 +1421,7 @@
     function checkToken(token) {
       var network = new Lampa.Reguest();
       network.timeout(8000);
-      network.silent(toProxy(Lampa.Utils.addUrlComponent(api_url + 'user_profile', dev_token + token)), function(json) {
+      network.silent(Lampa.Utils.addUrlComponent(api_url + 'user_profile', dev_token + token), function(json) {
         if (json) {
           if (json.user_data) {
             if (json.user_data.is_pro) window.fxapi.max_qualitie = 1080;
